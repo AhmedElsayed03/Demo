edfc5b0752a0c5616a22974a92282df22b541d55
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2024-09-24 11:42:06 +0300

    Register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 export const routes: Routes = [
-    {path: 'Login' , component:LoginComponent}
+    {path: 'Login' , component:LoginComponent},
+    {path : 'Register' , component:RegisterComponent}
 ];
