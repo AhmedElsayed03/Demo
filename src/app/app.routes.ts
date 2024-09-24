29051c8e022ce540acd43e7efb0a96dd95ebeeae
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2024-09-24 16:10:05 +0300

    Adding part of the user requests page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UserRequestsComponent } from './user-requests/user-requests.component';
 
 
 export const routes: Routes = [
     {path: 'Login' , component:LoginComponent},
-    {path : 'Register' , component:RegisterComponent}
+    {path : 'Register' , component:RegisterComponent},
+    {path : 'UserRequests' , component:UserRequestsComponent}
 ];
