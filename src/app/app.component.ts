a4877fffb195dc7018ce5648c45fc3ab19e76b46
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2024-09-25 13:51:23 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent ,RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
