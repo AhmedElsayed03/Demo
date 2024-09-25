6f5d2ea2340757107f0eb778088c227f5090be7d
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2024-09-25 11:46:25 +0300

    adding edit request + get request by id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserRequestsComponent } from './user-requests/user-requests.component';
 import { EditRequestDataComponent } from './edit-request-data/edit-request-data.component';
+import { GetRequestComponent } from './get-request/get-request.component';
 
 
 export const routes: Routes = [
     {path: 'Login' , component:LoginComponent},
     {path : 'Register' , component:RegisterComponent},
     {path : 'UserRequests' , component:UserRequestsComponent},
-    {path : 'EditRequest' , component: EditRequestDataComponent}
+    {path : 'EditRequest' , component: EditRequestDataComponent},
+    {path : 'getRequest', component:GetRequestComponent}
 ];
